8c293dc814c35ea0bbca8ad95ac21d557baa8265
zhoucheng <zhoucheng133@outlook.com>
2025-11-21 17:12:41 +0800

    添加动态库函数

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
+import "C"
+import "server_transfer_core/utils"
+
+//export SSHLogin
+func SSHLogin(url *C.char, port *C.char, username *C.char, password *C.char) *C.char {
+	return C.CString(utils.SshLogin(C.GoString(url), C.GoString(port), C.GoString(username), C.GoString(password)))
+}
+
+//export SftpList
+func SftpList(path *C.char) *C.char {
+	return C.CString(utils.SftpGetList(C.GoString(path)))
+}
+
+//export Disconnect
+func Disconnect() *C.char {
+	return C.CString(utils.Disconnect())
+}
+
 func main() {}
